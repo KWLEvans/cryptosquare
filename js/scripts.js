89a22230495f7f5f1fa440872d7616481a155966
Brenda Farrier and Keith Evans <keith@>
2017-01-19 13:44:02 -0800

    finish encryption for string

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,48 @@
 //Back-end
 function normalizeInput(input,sideLength) {
   var result = "";
-  for (i = input.length; i < (sideLength * sideLength); i++) {
+  console.log("sideLength: " + sideLength);
+  for (i = input.length; i < Math.pow(sideLength, 2); i++) {
     result += " ";
   }
   result = input + result;
+  console.log(result + "!");
+  console.log(input.length);
   return result;
+}
+
+function otherLoop(index, array) {
+  var newString = "";
+  array.forEach(function(item) {
+    newString += item[index];
+  });
+  return newString;
 }
 
 function buildEncrypted(input, sideLength) {
   var returnedString = "";
   var firstArray = [];
-  var encryptArray = [];
-  input = normalizeInput(input);
-  debugger;
-  for (var i = 0; i < input.length; i += sideLength) {
+  var newString = "";
+  input = normalizeInput(input, sideLength);
+  console.log(input + "!");
+
+  for (var i = 0; i < input.length; i = i + sideLength) {
     var sideString = input.slice(i, i + sideLength);
+    console.log("loop " + i + ": " + sideString);
     firstArray.push(sideString);
   }
-  debugger;
-  for (var i = 0; i < sideLength; i++) {
-    var newString = "";
-    debugger;
-    for (var j = 0; j < sideLength; i++) {
-      newString += firstArray[j].charAt(i);
-    }
-    encryptArray.push(newString);
-  }
-  debugger;
-  for (var i = 0; i < encryptArray.length; i++) {
-    returnedString += encryptArray[i];
-  }
+  console.log(firstArray);
 
-  return returnedString;
+  for (var i = 0; i < sideLength; i++) {
+      newString += otherLoop(i, firstArray);
+  }
+  return newString;
 }
 
 function encrypt(input) {
   var result = input.replace(/\W*/gi, "");
-  var square = Math.ceil(Math.sqrt(input.length));
-  final = buildEncrypted(result, square);
+  var square = Math.ceil(Math.sqrt(result.length));
+  var final = buildEncrypted(result, square);
 
   return final.toLowerCase();
 }
