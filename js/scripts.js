c0b2e16bd00c009828881e2bc1908482e72b4653
Brenda Farrier and Keith Evans <brenda@>
2017-01-19 11:43:07 -0800

    add possible square encryption functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,42 @@
 //Back-end
-function buildEncryptArray(input, sideLength) {
+function normalizeInput(input,sideLength) {
+  for (i = input.length; i < (sideLength * sideLength); i++) {
+    input = input + " ";
+  }
+}
+
+function buildEncrypted(input, sideLength) {
+  var returnedString = "";
+  var firstArray = [];
   var encryptArray = [];
+  input = normalizeInput(input);
+
   for (var i = 0; i < input.length; i += sideLength) {
     var sideString = input.slice(i, i + sideLength);
-    encryptArray.push(sideString);
+    firstArray.push(sideString);
   }
-  return encryptArray;
+
+  for (var i = 0; i < sideLength; i++) {
+    var newString = "";
+    for (var j = 0; j < sideLength; i++) {
+      newString += firstArray[j].charAt(i);
+    }
+    encryptArray.push(newString);
+  }
+
+  for (var i = 0; i < encryptArray.length; i++) {
+    returnedString += encryptArray[i];
+  }
+
+  return returnedString;
 }
 
 function encrypt(input) {
   var result = input.replace(/\W*/gi, "");
   var square = Math.ceil(Math.sqrt(input.length));
+  final = buildEncrypted(result, square);
 
-  return result.toLowerCase();
+  return final.toLowerCase();
 }
 
 //Front-end
